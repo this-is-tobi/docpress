837067105d8523732ad5a14e384adc381a1c140f
this-is-tobi <this-is-tobi@proton.me>
2024-11-11 23:09:33 +0100

    refactor: remove dead code

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -59,32 +59,6 @@ export function parseOptions<T extends Cmd[]>(cmds: [...T], opts: MergeOptions<T
   return mergedResult as MergeOptions<T>
 }
 
-// export function parseOptions<T extends Cmd | Cmd[]>(cmds: T, opts: T extends Cmd[] ? MergeOptions<T> : Options[T]): T extends Cmd[] ? MergeOptions<T> : Options[T] {
-//   log(`Initializing Docpress...`, 'info', 'blue')
-//   log(`\n\n-> Checking for required environment settings and configurations.`, 'info')
-
-//   function parseSingleOption<K extends Cmd>(cmd: K, singleOpts: Options[K]): Options[K] {
-//     const res = options[cmd].safeParse({
-//       ...globalOptsSchema.parse(singleOpts).config,
-//       ...singleOpts,
-//     })
-//     if (res.success) {
-//       return res.data as Options[K]
-//     }
-//     log(`   An error occurred while checking configuration: ${fromError(res.error).toString()}`, 'error')
-//     process.exit(0)
-//   }
-
-//   const cmdArray = Array.isArray(cmds) ? cmds : [cmds]
-//   const mergedResult = cmdArray.reduce((acc, singleCmd) => {
-//     const singleResult = parseSingleOption(singleCmd, opts as Options[typeof singleCmd])
-//     return Object.assign(acc, singleResult)
-//   }, {})
-
-//   log('   Setup complete! Ready to process your documentation.', 'info')
-//   return mergedResult as T extends Cmd[] ? MergeOptions<T> : Options[T]
-// }
-
 export function addOptions(cmd: Command, opts: Option[]) {
   opts.forEach(opt => cmd.addOption(opt))
   return cmd
